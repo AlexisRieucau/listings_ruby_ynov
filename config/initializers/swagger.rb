2e283aec986f638c3c4ef08bb139a5d645b872ba
Alexis Rieucau <alexis.rieucau@gmail.com>
2019-05-18 18:06:01 +0200

    changed swagger doc url

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,2 +1,2 @@
 GrapeSwaggerRails.options.url      = '/api/v1/swagger_doc'
-GrapeSwaggerRails.options.app_url  = 'http://localhost:3000'
+GrapeSwaggerRails.options.app_url  = 'https://fathomless-island-36787.herokuapp.com'
